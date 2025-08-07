0a42f28cb4dfde97e65523ae52eaf0ffc9223bc0
codebylalit <lalitkumarnamdev4645@gmail.com>
2025-08-07 14:55:41 +0530

    loader updt

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./LoadingSpinner.css";
 
-const LoadingSpinner = () => {
-  return (
-    <div className="loading-container">
-      <div className="loading-spinner">
-        <div className="spinner-ring"></div>
-        <div className="spinner-ring"></div>
-        <div className="spinner-ring"></div>
-      </div>
-      <p className="loading-text">Loading weather data...</p>
+const LoadingSpinner = () => (
+  <div className="loading-container">
+    <div className="loading-spinner">
+      <div className="spinner-ring"></div>
+      <div className="spinner-ring"></div>
+      <div className="spinner-ring"></div>
     </div>
-  );
-};
+    <p className="loading-text">Loading weather data...</p>
+  </div>
+);
 
 export default LoadingSpinner;
